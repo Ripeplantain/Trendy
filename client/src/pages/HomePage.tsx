2f6c845a3f4940b0a48d42a951eca616a6180ff4
ripeplantain <irvingmanny@gmail.com>
2023-09-11 21:39:54 +0000

    fixed responsive padding

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -4,12 +4,12 @@ const HomePage = () => {
   return (
     <div className='bg-gray-100 dark:bg-gray-950 h-[100%] font-[Rubik]'>
       <Navbar />
-      <div className='lg:flex pt-[7rem] pb-9 px-8 md:px-[5rem] gap-[3vw]'>
+      <div className='lg:flex pt-[7rem] pb-9  md:px-[5rem] gap-[3vw]'>
         <div className='basis-[30%] flex flex-col gap-8 px-7'>
           <Profile />
           <Info />
         </div>
-        <div className='mt-10 lg:mt-0 basis-[70%]'>
+        <div className='mt-10 lg:mt-0 basis-[70%] px-8'>
           <Feed />
         </div>
       </div>
