7e5e97a02ede34ef677fd76fc5d079063991d95f
ripeplantain <irvingmanny@gmail.com>
2023-09-23 12:54:40 +0000

    done

--- client/src/pages/Auth/LoginPage.tsx
+++ client/src/pages/Auth/LoginPage.tsx
@@ -5,7 +5,7 @@ const LoginPage = () => {
   return (
     <div className='font-[Lora] overflow-hidden h-fit'>
       <header className='text-center py-6 border-b dark:border-white bg-white dark:bg-gray-950 dark:text-white'>
-        <h1 className='tracking-wider text-4xl md:text-3xl font-bold text-orange-700'>Trendy</h1>
+        <h1 className='tracking-wider text-4xl md:text-3xl font-bold text-orange-700'>Monty Gram</h1>
       </header>
       <Forms />
     </div>
