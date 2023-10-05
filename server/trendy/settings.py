b9009b1dcfe3eeff213619eec685c7bfcba2e5a2
ripeplantain <irvingmanny@gmail.com>
2023-10-05 10:02:54 +0000

    production

--- server/trendy/settings.py
+++ server/trendy/settings.py
@@ -2,7 +2,7 @@
 from pathlib import Path
 
 import os
-import dj_database_url
+# import dj_database_url
 from datetime import timedelta
 
 from dotenv import load_dotenv
@@ -21,7 +21,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1").split("  ")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1").split(" ")
 
 
 # Application definition
