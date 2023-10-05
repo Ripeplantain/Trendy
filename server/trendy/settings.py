f8d9534b477f1db27a408fd7b8aa45bf7af53d8b
ripeplantain <irvingmanny@gmail.com>
2023-10-05 14:08:19 +0000

    done

--- server/trendy/settings.py
+++ server/trendy/settings.py
@@ -228,3 +228,11 @@ CHANNEL_LAYERS = {
         },
     },
 } 
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get("CLOUD_NAME"),
+    'API_KEY': os.environ.get("API_KEY"),
+    'API_SECRET': os.environ.get("API_SECRET"),
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
